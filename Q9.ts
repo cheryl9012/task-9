8a2b78a14b4e1ae87bde7363e889aa0452c098e9
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-20 19:16:25 +0500

    Add files via upload
    In this task, the favorite number has been stored.

---
+++ Q9.ts
@@ -0,0 +1,4 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let num1: number = 9012
+console.log(`my favorite number is  ${num1}`);
